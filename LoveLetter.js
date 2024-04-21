72ac1cc3ee85b1264d8b75f21abd530df6d8ed04
saisakti <75479052+saisakti@users.noreply.github.com>
2024-04-21 15:23:10 +0530

    Add files via upload

---
+++ LoveLetter.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from 'react';
+import './Letter.css';
+import audioFile from './sh.mp3';
+
+const Letter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        Nannaaa Sravanii...🖤,<br />
+        enduku antha kopam?kopam chiraku eedi anna parla tittu anthe ganii matladakunda reply evvakunda undaku <br />plsss 🤌🏻:)<br />
+        always sh..❤️‍🩹,<br />
+        @Saiiii
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default Letter;
